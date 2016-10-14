9cebdcc3946de86496a0ea5ee0ca4f92fde583ef
Bilbo <listda7921@gmail.com>
2016-10-14 00:13:09 -0500

    api points to modulus server

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,44 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope, $http) {
+  $scope.title = "Things To Do";
+  $scope.formData = {text:''};
+  $http.get('http://nodeapp-91759.onmodulus.net/api/todos')
+        .success(function(data) {
+            $scope.todos = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            alert('Error: ' + data);
+            console.log('Error: ' + data);
+        });
+
+    // when submitting the add form, send the text to the node API
+    $scope.createTodo = function() {
+        console.log($scope.formData);
+        $http.post('http://nodeapp-91759.onmodulus.net/api/todos', $scope.formData)
+            .success(function(data) {
+                $scope.formData = {}; // clear the form so our user is ready to enter another
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+    // delete a todo after checking it
+    $scope.deleteTodo = function(id) {
+        $http.delete('http://nodeapp-91759.onmodulus.net/api/todos/' + id)
+            .success(function(data) {
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+})
 
 .controller('ChatsCtrl', function($scope, Chats, $http) {
   $http.get('192.168.1.112:3000/api/todos')
